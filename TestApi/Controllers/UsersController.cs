6b833311a225c2070d2c912498a204f90f8fe46b
Lionel Dabee <dabee.lionel@gmail.com>
2021-09-14 17:50:30 +0200

    Ajoutez des fichiers projet.

---
+++ TestApi/Controllers/UsersController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TestApi.Models;
+using TestApi.Repository;
+using System.Web.Http.Cors;
+
+
+namespace TestApi.Controllers
+{
+    [EnableCors(origins: "https://localhost:44388", headers: "*", methods: "*")]
+
+    [RoutePrefix("api")]
+    public class UsersController : ApiController
+    {
+        [EnableCors("*", "*", "*")]
+        [HttpGet, HttpPost]
+        [Route("allUsers")]
+        public List<Users> GetAllUsers()
+        {
+            return UsersRepository.GetAllUsers();
+        }
+
+        [EnableCors("*", "*", "*")]
+        [HttpPost]
+        [Route("insertUser")]
+        public List<Users> InsertUser([FromBody] Users user)
+        {
+            return UsersRepository.InsertUser(user);
+        }
+
+        [EnableCors("*", "*", "*")]
+        [HttpPost]
+        [Route("updatetUser")]
+        public List<Users> UpdateUser([FromBody] Users user)
+        {
+            return UsersRepository.UpdateUser(user);
+        }
+
+        [EnableCors("*", "*", "*")]
+        [HttpPost]
+        [Route("deletetUser")]
+        public List<Users> DeleteUser([FromBody] Users user)
+        {
+            return UsersRepository.DeleteUser(user);
+        }
+
+    }
+}
---
+++ TestApi/Repository/UsersRepository.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using TestApi.Models;
+using System.Data;
+
+namespace TestApi.Repository
+{
+    public class UsersRepository
+    {
+        public static List<Users> GetAllUsers()
+        {
+            List<Users> People = new List<Users>();
+
+            using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["testapi"].ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("Users_GetAll", connection))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    connection.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while(reader.Read())
+                    {
+                        Users user = new Users()
+                        {
+                            Id = Convert.ToInt32(reader["Id"]),
+                            LastName = reader["LastName"]?.ToString(),
+                            FirstName = reader["FirstName"]?.ToString(),
+                        };
+                        People.Add(user);
+
+                    }
+                    connection.Close();
+                }
+            }
+
+            return People;
+        }
+
+        public static List<Users> InsertUser(Users User)
+        {
+            List<Users> People = new List<Users>();
+            using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["testapi"].ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("Users_Insert",connection))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("FirstName", User.FirstName);
+                    cmd.Parameters.AddWithValue("LastName", User.LastName);
+                    connection.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while(reader.Read())
+                    {
+                        Users user = new Users()
+                        {
+                            Id = Convert.ToInt32(reader["Id"]),
+                            LastName = reader["LastName"]?.ToString(),
+                            FirstName = reader["FirstName"]?.ToString(),
+                        };
+                        People.Add(user);
+                    }
+                    connection.Close();
+                }
+            }
+            return People;
+        }
+
+        public static List<Users> UpdateUser(Users User)
+        {
+            List<Users> People = new List<Users>();
+            using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["testapi"].ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("Users_Update", connection))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("FirstName", User.FirstName);
+                    cmd.Parameters.AddWithValue("LastName", User.LastName);
+                    cmd.Parameters.AddWithValue("Id", User.Id);
+                    connection.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Users user = new Users()
+                        {
+                            Id = Convert.ToInt32(reader["Id"]),
+                            LastName = reader["LastName"]?.ToString(),
+                            FirstName = reader["FirstName"]?.ToString(),
+                        };
+                        People.Add(user);
+                    }
+                    connection.Close();
+                }
+            }
+            return People;
+        }
+
+        public static List<Users> DeleteUser(Users User)
+        {
+            List<Users> People = new List<Users>();
+            using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["testapi"].ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("Users_Delete", connection))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("Id", User.Id);
+                    connection.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Users user = new Users()
+                        {
+                            Id = Convert.ToInt32(reader["Id"]),
+                            LastName = reader["LastName"]?.ToString(),
+                            FirstName = reader["FirstName"]?.ToString(),
+                        };
+                        People.Add(user);
+                    }
+                    connection.Close();
+                }
+            }
+            return People;
+        }
+
+
+    }
+
+}
